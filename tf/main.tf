ce42c5754f04c2641a2b1ecef467a67c13a4f00c
rad <radoslaw.brus@gmail.com>
2023-04-16 15:55:56 +0200

    Max

--- tf/main.tf
+++ tf/main.tf
@@ -1,10 +1,62 @@
+variable "environment" {
+  type = object({
+    name = string
+  })
+  nullable = false
+}
+
+variable "vaultConfig" {
+  type = object({
+    address = string
+    token = string
+  })
+  nullable = false
+}
+
+variable "envs" {
+  type = list
+  nullable = false
+}
+
+variable "frontend" {
+  type = object({
+    imageName = string
+    ports = object({
+        internal = number
+        external = number
+  })
+  })
+  nullable = false
+}
+
+variable "account" {
+  type = object({
+    password = string
+  })
+  nullable = false
+}
+
+variable "gateway" {
+  type = object({
+    password = string
+  })
+  nullable = false
+}
+
+variable "payment" {
+  type = object({
+    password = string
+  })
+  nullable = false
+}
+
 terraform {
   required_version = ">= 1.0.7"
 
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.15.0"
+      version = "3.0.2"
     }
 
     vault = {
@@ -13,25 +65,27 @@ terraform {
   }
 }
 
-provider "vault" {
-  address = "http://localhost:8201"
-  token   = "f23612cf-824d-4206-9e94-e31a6dc8ee8d"
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
 
-provider "vault" {
-  alias   = "vault_dev"
-  address = "http://localhost:8201"
-  token   = "f23612cf-824d-4206-9e94-e31a6dc8ee8d"
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
 }
 
+#resource "docker_container" "foo" {
+#  image = docker_image.ubuntu.image_id
+#  name  = "foo"
+#}
+
 provider "vault" {
-  alias   = "vault_prod"
-  address = "http://localhost:8301"
-  token   = "083672fc-4471-4ec4-9b59-a285e463a973"
+  alias   = "secret_store"
+  address = var.vaultConfig.address
+  token   = var.vaultConfig.token
 }
 
-resource "vault_audit" "audit_dev" {
-  provider = vault.vault_dev
+resource "vault_audit" "audit" {
+  provider = vault.secret_store
   type     = "file"
 
   options = {
@@ -39,45 +93,30 @@ resource "vault_audit" "audit_dev" {
   }
 }
 
-resource "vault_audit" "audit_prod" {
-  provider = vault.vault_prod
-  type     = "file"
-
-  options = {
-    file_path = "/vault/logs/audit"
-  }
-}
-
-resource "vault_auth_backend" "userpass_dev" {
-  provider = vault.vault_dev
-  type     = "userpass"
-}
-
-
-resource "vault_auth_backend" "userpass_prod" {
-  provider = vault.vault_prod
+resource "vault_auth_backend" "userpass" {
+  provider = vault.secret_store
   type     = "userpass"
 }
 
 resource "vault_generic_secret" "account_development" {
-  provider = vault.vault_dev
-  path     = "secret/development/account"
+  provider = vault.secret_store
+  path     = "secret/${var.environment.name}/account"
 
   data_json = <<EOT
 {
   "db_user":   "account",
-  "db_password": "965d3c27-9e20-4d41-91c9-61e6631870e7"
+  "db_password": ${var.account.password}
 }
 EOT
 }
 
 resource "vault_policy" "account_development" {
-  provider = vault.vault_dev
-  name     = "account-development"
+  provider = vault.secret_store
+  name     = "account-${var.environment.name}"
 
   policy = <<EOT
 
-path "secret/data/development/account" {
+path "secret/data/${var.environment.name}/account" {
     capabilities = ["list", "read"]
 }
 
@@ -85,38 +124,38 @@ EOT
 }
 
 resource "vault_generic_endpoint" "account_development" {
-  provider             = vault.vault_dev
-  depends_on           = [vault_auth_backend.userpass_dev]
-  path                 = "auth/userpass/users/account-development"
+  provider             = vault.secret_store
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/account-${var.environment.name}"
   ignore_absent_fields = true
 
   data_json = <<EOT
 {
-  "policies": ["account-development"],
-  "password": "123-account-development"
+  "policies": ["account-${var.environment.name}"],
+  "password": "123-account-${var.environment.name}"
 }
 EOT
 }
 
 resource "vault_generic_secret" "gateway_development" {
-  provider = vault.vault_dev
-  path     = "secret/development/gateway"
+  provider = vault.secret_store
+  path     = "secret/${var.environment.name}/gateway"
 
   data_json = <<EOT
 {
   "db_user":   "gateway",
-  "db_password": "10350819-4802-47ac-9476-6fa781e35cfd"
+  "db_password": ${var.gateway.password}
 }
 EOT
 }
 
 resource "vault_policy" "gateway_development" {
-  provider = vault.vault_dev
-  name     = "gateway-development"
+  provider = vault.secret_store
+  name     = "gateway-${var.environment.name}"
 
   policy = <<EOT
 
-path "secret/data/development/gateway" {
+path "secret/data/${var.environment.name}/gateway" {
     capabilities = ["list", "read"]
 }
 
@@ -124,37 +163,37 @@ EOT
 }
 
 resource "vault_generic_endpoint" "gateway_development" {
-  provider             = vault.vault_dev
-  depends_on           = [vault_auth_backend.userpass_dev]
-  path                 = "auth/userpass/users/gateway-development"
+  provider             = vault.secret_store
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/gateway-${var.environment.name}"
   ignore_absent_fields = true
 
   data_json = <<EOT
 {
-  "policies": ["gateway-development"],
-  "password": "123-gateway-development"
+  "policies": ["gateway-${var.environment.name}"],
+  "password": "123-gateway-${var.environment.name}"
 }
 EOT
 }
 resource "vault_generic_secret" "payment_development" {
-  provider = vault.vault_dev
-  path     = "secret/development/payment"
+  provider = vault.secret_store
+  path     = "secret/${var.environment.name}/payment"
 
   data_json = <<EOT
 {
   "db_user":   "payment",
-  "db_password": "a63e8938-6d49-49ea-905d-e03a683059e7"
+  "db_password": ${var.payment.password}
 }
 EOT
 }
 
 resource "vault_policy" "payment_development" {
-  provider = vault.vault_dev
-  name     = "payment-development"
+  provider = vault.secret_store
+  name     = "payment-${var.environment.name}"
 
   policy = <<EOT
 
-path "secret/data/development/payment" {
+path "secret/data/${var.environment.name}/payment" {
     capabilities = ["list", "read"]
 }
 
@@ -162,227 +201,27 @@ EOT
 }
 
 resource "vault_generic_endpoint" "payment_development" {
-  provider             = vault.vault_dev
-  depends_on           = [vault_auth_backend.userpass_dev]
-  path                 = "auth/userpass/users/payment-development"
+  provider             = vault.secret_store
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/payment-${var.environment.name}"
   ignore_absent_fields = true
 
   data_json = <<EOT
 {
-  "policies": ["payment-development"],
-  "password": "123-payment-development"
+  "policies": ["payment-${var.environment.name}"],
+  "password": "123-payment-${var.environment.name}"
 }
 EOT
 }
 
-resource "vault_generic_secret" "account_production" {
-  provider = vault.vault_prod
-  path     = "secret/production/account"
-
-  data_json = <<EOT
-{
-  "db_user":   "account",
-  "db_password": "396e73e7-34d5-4b0a-ae1b-b128aa7f9977"
-}
-EOT
-}
-
-resource "vault_policy" "account_production" {
-  provider = vault.vault_prod
-  name     = "account-production"
-
-  policy = <<EOT
-
-path "secret/data/production/account" {
-    capabilities = ["list", "read"]
-}
-
-EOT
-}
-
-resource "vault_generic_endpoint" "account_production" {
-  provider             = vault.vault_prod
-  depends_on           = [vault_auth_backend.userpass_prod]
-  path                 = "auth/userpass/users/account-production"
-  ignore_absent_fields = true
-
-  data_json = <<EOT
-{
-  "policies": ["account-production"],
-  "password": "123-account-production"
-}
-EOT
-}
-
-resource "vault_generic_secret" "gateway_production" {
-  provider = vault.vault_prod
-  path     = "secret/production/gateway"
-
-  data_json = <<EOT
-{
-  "db_user":   "gateway",
-  "db_password": "33fc0cc8-b0e3-4c06-8cf6-c7dce2705329"
-}
-EOT
-}
-
-resource "vault_policy" "gateway_production" {
-  provider = vault.vault_prod
-  name     = "gateway-production"
-
-  policy = <<EOT
-
-path "secret/data/production/gateway" {
-    capabilities = ["list", "read"]
-}
-
-EOT
-}
-
-resource "vault_generic_endpoint" "gateway_production" {
-  provider             = vault.vault_prod
-  depends_on           = [vault_auth_backend.userpass_prod]
-  path                 = "auth/userpass/users/gateway-production"
-  ignore_absent_fields = true
-
-  data_json = <<EOT
-{
-  "policies": ["gateway-production"],
-  "password": "123-gateway-production"
-}
-EOT
-}
-
-resource "vault_generic_secret" "payment_production" {
-  provider = vault.vault_prod
-  path     = "secret/production/payment"
-
-  data_json = <<EOT
-{
-  "db_user":   "payment",
-  "db_password": "821462d7-47fb-402c-a22a-a58867602e39"
-}
-EOT
-}
-
-resource "vault_policy" "payment_production" {
-  provider = vault.vault_prod
-  name     = "payment-production"
-
-  policy = <<EOT
-
-path "secret/data/production/payment" {
-    capabilities = ["list", "read"]
-}
-
-EOT
-}
-
-resource "vault_generic_endpoint" "payment_production" {
-  provider             = vault.vault_prod
-  depends_on           = [vault_auth_backend.userpass_prod]
-  path                 = "auth/userpass/users/payment-production"
-  ignore_absent_fields = true
-
-  data_json = <<EOT
-{
-  "policies": ["payment-production"],
-  "password": "123-payment-production"
-}
-EOT
-}
-
-resource "docker_container" "account_production" {
-  image = "form3tech-oss/platformtest-account"
-  name  = "account_production"
-
-  env = [
-    "VAULT_ADDR=http://vault-production:8200",
-    "VAULT_USERNAME=account-production",
-    "VAULT_PASSWORD=123-account-production",
-    "ENVIRONMENT=production"
-  ]
-
-  networks_advanced {
-    name = "vagrant_production"
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
-resource "docker_container" "gateway_production" {
-  image = "form3tech-oss/platformtest-gateway"
-  name  = "gateway_production"
-
-  env = [
-    "VAULT_ADDR=http://vault-production:8200",
-    "VAULT_USERNAME=gateway-production",
-    "VAULT_PASSWORD=123-gateway-production",
-    "ENVIRONMENT=production"
-  ]
-
-  networks_advanced {
-    name = "vagrant_production"
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
-resource "docker_container" "payment_production" {
-  image = "form3tech-oss/platformtest-payment"
-  name  = "payment_production"
-
-  env = [
-    "VAULT_ADDR=http://vault-production:8200",
-    "VAULT_USERNAME=payment-production",
-    "VAULT_PASSWORD=123-payment-production",
-    "ENVIRONMENT=production"
-  ]
-
-  networks_advanced {
-    name = "vagrant_production"
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
-resource "docker_container" "frontend_production" {
-  image = "docker.io/nginx:1.22.0-alpine"
-  name  = "frontend_production"
-
-  ports {
-    internal = 80
-    external = 4081
-  }
-
-  networks_advanced {
-    name = "vagrant_production"
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
 
 resource "docker_container" "account_development" {
   image = "form3tech-oss/platformtest-account"
-  name  = "account_development"
-
-  env = [
-    "VAULT_ADDR=http://vault-development:8200",
-    "VAULT_USERNAME=account-development",
-    "VAULT_PASSWORD=123-account-development",
-    "ENVIRONMENT=development"
-  ]
+  name  = "account"
+  env = var.envs
 
   networks_advanced {
-    name = "vagrant_development"
+    name = "vagrant"
   }
 
   lifecycle {
@@ -392,17 +231,11 @@ resource "docker_container" "account_development" {
 
 resource "docker_container" "gateway_development" {
   image = "form3tech-oss/platformtest-gateway"
-  name  = "gateway_development"
-
-  env = [
-    "VAULT_ADDR=http://vault-development:8200",
-    "VAULT_USERNAME=gateway-development",
-    "VAULT_PASSWORD=123-gateway-development",
-    "ENVIRONMENT=development"
-  ]
+  name  = "gateway"
+  env = var.envs
 
   networks_advanced {
-    name = "vagrant_development"
+    name = "vagrant"
   }
 
   lifecycle {
@@ -412,17 +245,11 @@ resource "docker_container" "gateway_development" {
 
 resource "docker_container" "payment_development" {
   image = "form3tech-oss/platformtest-payment"
-  name  = "payment_development"
-
-  env = [
-    "VAULT_ADDR=http://vault-development:8200",
-    "VAULT_USERNAME=payment-development",
-    "VAULT_PASSWORD=123-payment-development",
-    "ENVIRONMENT=development"
-  ]
+  name  = "payment"
+  env = var.envs
 
   networks_advanced {
-    name = "vagrant_development"
+    name = "vagrant"
   }
 
   lifecycle {
@@ -431,16 +258,16 @@ resource "docker_container" "payment_development" {
 }
 
 resource "docker_container" "frontend_development" {
-  image = "docker.io/nginx:latest"
-  name  = "frontend_development"
+  image = var.frontend.imageName
+  name  = "frontend"
 
   ports {
-    internal = 80
-    external = 4080
+    internal = var.frontend.ports.internal
+    external = var.frontend.ports.external
   }
 
   networks_advanced {
-    name = "vagrant_development"
+    name = "vagrant"
   }
 
   lifecycle {
